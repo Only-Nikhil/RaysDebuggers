9d005ca706f8ca580fc80d728c2cee87b905b48e
shivam-singh-coder <shivamsinghballia2003@gmail.com>
2024-10-19 05:36:39 +0530

    Modify Till 5:30 AM

---
+++ JS/mentorship.js
@@ -0,0 +1,262 @@
+// data for creating mentors slots
+export const allmentors = [
+    {
+        id: 1,
+        image: "/Media/Dr_Birmohan_singh.jpg",
+        name: "Dr. Birmohan Singh",
+        typeOfRespond: "Quick Responder",
+        designation: "Professor",
+        where:"SLIET",
+        experience: "Teaching expert with 30 years experience in Software Engineering,Operating system,System Software....",
+        rating: {
+            star: "5",
+            review: "76"
+        },
+        guidance: ["Software Engineering", "Operating system", "System Software", "Object-Oriented programing", "System Analysis and Design", "Data Structures", "Computer Graphics"],
+        availableSlots: ["10:00 AM - 11:00 AM", "11:00 AM - 12:00 PM", "1:00 PM"],
+        bookedSlots: [] // this will keep booked slots
+    },
+    {
+        id: 2,
+        image: "/Media/Dr_Damanpreet_singh.jpg",
+        name: "Dr. Damanpreet Singh",
+        typeOfRespond: "Top Mentor",
+        designation: "Professor",
+        where:"SLIET",
+        experience: "Teaching expert with 25 years experience and role as Associate Dean at SLIET",
+        rating: {
+            star: "4",
+            review: "25"
+        },
+        guidance: ["Computer Science & Engineering"],
+        availableSlots: ["10:00 AM - 11:00 AM", "11:00 AM - 12:00 PM", "1:00 PM"],
+        bookedSlots: [] // this will keep booked slots
+    },
+    {
+        id: 3,
+        image: "/Media/Dr_Gurjinder_Kaur.jpg",
+        name: "Dr. Gurjinder Kaur",
+        typeOfRespond: "Top Mentor",
+        designation: "Associate Professor",
+        where:"SLIET",
+        experience: "Joined as Lecturer in August 1997 at SLIET and presently working as Associate Professor.",
+        rating: {
+            star: "4",
+            review: "22"
+        },
+        guidance: ["Course counsellor","Academic Activities"],
+        availableSlots: ["2:00 PM", "4:00 PM - 5:00 PM"],
+        bookedSlots: [] // this will keep booked slots
+    },
+    {
+        id: 4,
+        image: "/Media/Dr_Jagdeep_Singh.jpg",
+        name: "Dr. Jagdeep Singhr",
+        typeOfRespond: "Quick Responder",
+        designation: "Assistant Professor",
+        where:"SLIET",
+        experience: "Assistant Professor in the Department of Computer Science and Engineering at SLIET & Teaching-Cum-Research Fellow in the Division of Information Technology at NSIT",
+        rating: {
+            star: "5",
+            review: "56"
+        },
+        guidance: ["UGC-NET","GATE","Electronics and Information Technology"],
+        availableSlots: ["2:00 PM", "4:00 PM - 5:00 PM"],
+        bookedSlots: [] // this will keep booked slots
+    },
+];
+// show mentors slots
+function showAllMentors() {
+    const mentorList = document.getElementById("mentor-list");
+    if (mentorList !== null) {
+        mentorList.innerHTML = ""; // Clear all prevoius content
+    }
+    else {
+        alert("Something went wrong!!")
+        return false;
+    }
+    allmentors.forEach(mentor => {
+        // Create mentor div
+        const mentorCard = document.createElement("div");
+        mentorCard.classList.add("mentor-slide");
+        const bookedSlotsLen = mentor.bookedSlots.length;
+        var topMsg='';
+        if(bookedSlotsLen === 0){
+            topMsg= `<div>
+                        <img src="/Media/slot-machine.png" alt="slot-icon">
+                    </div>
+                    Only ${mentor.availableSlots.length} Slots Left`;
+        }
+        else if(bookedSlotsLen === mentor.availableSlots.length){
+            topMsg = `<div>
+            <img src="/Media/slot-machine.png" alt="slot-icon"></div>
+            Booked`;
+        }
+        else{
+            topMsg = `<div>
+            <img src="/Media/slot-machine.png" alt="slot-icon"></div>
+            ${parseInt(mentor.availableSlots.length)-parseInt(bookedSlotsLen)} Slots Left`;
+        }
+        if(topMsg !==''){
+            const topMsgDiv = document.createElement("div");
+            topMsgDiv.classList.add("legend-top");
+            if (topMsg.includes('Booked')) {
+                topMsgDiv.classList.add("booked")
+            }
+            topMsgDiv.innerHTML = topMsg;
+            mentorCard.appendChild(topMsgDiv);
+        }
+        // create mentor start
+        const mentor_start = document.createElement('div');
+        mentor_start.classList.add('mentor-start');
+        // create mentor image
+        const mentorImageDiv = document.createElement("div");
+        mentorImageDiv.classList.add("mentor-image");
+        mentorImageDiv.innerHTML = `<a href="/Html/profile.html?id=${mentor.id}" style="position: relative;" data-mentor-id="${mentor.id}">
+                            <img src="${mentor.image}" alt="mentor pic" class="wid-100 h-100">
+                            <div></div>
+                        </a>`;
+        mentor_start.appendChild(mentorImageDiv);
+        // end mentor image
+        const mentor_description = document.createElement('div');
+        mentor_description.classList.add('mentor-description');
+        // create name
+        const nameDiv = document.createElement("div");
+        nameDiv.classList.add("m-name");
+        nameDiv.innerHTML = `<a href="/Html/profile.html?id=${mentor.id}" data-mentor-id="${mentor.id}">
+                                <h3>${mentor.name}</h3>
+                            </a>
+                            <div class="respond">
+                                    <img src="/Media/auto-reply.png" alt="">
+                                    ${mentor.typeOfRespond}
+                            </div>`
+        mentor_description.appendChild(nameDiv);
+        // end name
+        // create designation div
+        const desgDiv = document.createElement('div');
+        desgDiv.classList.add('m-deg');
+        desgDiv.innerHTML = `<span>${mentor.designation} at </span><b>${mentor.where}</b>`;
+        mentor_description.appendChild(desgDiv);
+        // end designation
+        // create experience
+        const expDiv = document.createElement('div');
+        expDiv.classList.add('m-exp');
+        expDiv.innerHTML = `<span>${mentor.experience}</span>`;
+        mentor_description.appendChild(expDiv);
+        // end experience
+        // create star rating
+        const starDivCont = document.createElement('div');
+        starDivCont.classList.add('m-star');
+        const starInnerDivCont = document.createElement('div');
+        for (let index = 0; index < parseInt(mentor.rating.star); index++) {
+            const imgElemnt = document.createElement('img');
+            imgElemnt.src = "/Media/star.png";
+            imgElemnt.alt = "star";
+            starInnerDivCont.appendChild(imgElemnt);            
+        }
+        starDivCont.appendChild(starInnerDivCont);
+        const spanStar = document.createElement('span');
+        spanStar.innerHTML = `<b>${mentor.rating.star}</b> (${mentor.rating.review} review)`;
+        starDivCont.appendChild(spanStar);
+        mentor_description.appendChild(starDivCont);
+        // end star rating
+        // create career section
+        const careerContainer = document.createElement("div");
+        careerContainer.classList.add("m-career");
+        careerContainer.innerHTML ="<h3>Career Guidelines: </h3";
+        const careerInnerContainer = document.createElement("div");
+        mentor.guidance.forEach(guide => {
+            const spanElemnt = document.createElement("span");
+            spanElemnt.textContent = guide;
+            careerInnerContainer.appendChild(spanElemnt);
+        });
+        // end creation of career section
+        // add inner slot conatiner in outer slot conatiner
+        careerContainer.appendChild(careerInnerContainer);
+        // add slot conatiner in mentor Card div
+        mentor_description.appendChild(careerContainer);
+        // show available slots
+        const slotsContainer = document.createElement("div");
+        slotsContainer.classList.add("m-slots");
+        slotsContainer.innerHTML ="<h3>Available Time Slots</h3>";
+        const slotInnerContainer = document.createElement("div");
+        mentor.availableSlots.forEach(slot => {
+            const slotButton = document.createElement("button");
+            slotButton.setAttribute('type','button');
+            slotButton.classList.add("slot");
+            slotButton.textContent = slot;
+
+            // Check if slot is already booked
+            if (mentor.bookedSlots.includes(slot)) {
+                slotButton.classList.add("booked");
+                slotButton.textContent = "Booked";
+            }
+
+            // Add event listener to book the slot
+            slotButton.addEventListener("click", () => {
+                bookSlot(mentor.id, slot);
+            });
+
+            slotInnerContainer.appendChild(slotButton);
+        });
+        // end creation of availableSlots
+        // add inner slot conatiner in outer slot conatiner
+        slotsContainer.appendChild(slotInnerContainer);
+        // add slot conatiner in mentor Card div
+        mentor_description.appendChild(slotsContainer);
+        // create profile view button
+        const profile_view = document.createElement("div");
+        profile_view.classList.add("view-div");
+        profile_view.innerHTML=`<div><span>Free</span></div> <a href="/Html/profile.html?id=${mentor.id}" data-mentor-id="${mentor.id}"><button type="button">View Profile</button></a>`;
+        // add profile button in mentor Card div
+        mentor_description.appendChild(profile_view);
+        mentor_start.appendChild(mentor_description);
+        mentorCard.appendChild(mentor_start);
+        // add mentor Card div in main mentor div
+        mentorList.appendChild(mentorCard);
+    });
+}
+
+// Book a slot
+export function bookSlot(mentorId, slot) {
+    console.log(mentorId);
+    console.log(slot);
+    
+    const mentor = allmentors.find(m => m.id === mentorId);
+
+    // Mark the slot as booked
+    mentor.bookedSlots.push(slot);
+
+    // Optionally, save booking data to localStorage
+    localStorage.setItem("selectedMentors", JSON.stringify(allmentors));
+    const pathUrl = window.location.href;
+    if (pathUrl.includes('mentorship.html')) { 
+        // Redisplay mentors with updated booking status
+        showAllMentors();
+    }
+    else{
+        return true;
+    }
+
+    // Show confirmation alert
+    alert(`Now,You have successfully booked ${slot} with ${mentor.name}.`);
+}
+
+// Load data from localStorage if available
+export function loadMentors() {
+    const storedMentors = localStorage.getItem("selectedMentors");
+    if (storedMentors) {
+        allmentors.forEach((mentor, index) => {
+            allmentors[index] = JSON.parse(storedMentors)[index];
+        });
+    }
+}
+
+// load content on page open
+const pathUrl = window.location.href;
+if (pathUrl.includes('mentorship.html')) { 
+    window.onload = () => {
+        loadMentors();
+        showAllMentors();
+    };
+}
--- JS/profile.js
+++ JS/profile.js
@@ -1,12 +1,103 @@
-const pdiv=document.querySelectorAll('.profile-side-main-div');
-pdiv.forEach((element) => {    
-    element.addEventListener('click', function (event) {
-        pdiv.forEach((element) => {
-            element.classList.remove('active');
-        })
-        this.classList.toggle('active');
+import {allmentors,loadMentors,bookSlot} from './mentorship.js';
+document.addEventListener('DOMContentLoaded',function(){
+    loadMentors();
+    const pdiv=document.querySelectorAll('.profile-side-main-div');
+    pdiv.forEach((element) => {    
+        element.addEventListener('click', function (event) {
+            const profile_section = document.querySelector('.profile-details');
+            if (profile_section !==null) {
+                const slotSection = profile_section.querySelector(".slot-box");
+                if (slotSection !==null) {
+                    const selecetd_mentor = this.dataset.mentorId;                    
+                    if (selecetd_mentor !== undefined) {  
+                        pdiv.forEach((element) => {
+                            element.classList.remove('active');
+                        })
+                        this.classList.toggle('active');
+                        const mentor = allmentors.find(m => m.id === parseInt(selecetd_mentor));
+                        showSlots(mentor,profile_section);     
+                    }
+                    else{
+                        alert("This mentor doesn't have mentor id!!")
+                    }             
+                }
+                else{
+                    alert("Something went wrong!!")
+                }
+            }
+        });
     });
+    // Get the current URL
+    const url = new URL(window.location.href);
+
+    // Extract the 'id' parameter
+    const id = url.searchParams.get('id');
+    if (id !== null) {
+        const profile_side_main_div = document.querySelectorAll('.profile-side-main-div');
+        if(profile_side_main_div.length !==0){
+            profile_side_main_div.forEach((profile)=>{
+                if(profile.dataset.mentorId === id){
+                    profile.click();
+                }
+            })            
+        }
+        // const mentor = allmentors.find(m => m.id === parseInt(id));
+        // if (mentor !== null) {
+        //     const profile_section = document.querySelector('.profile-details');
+        //     if (profile_section !==null) {
+        //         showSlots(mentor,profile_section)
+        //     }
+        // }
+    }
+    else{
+        const mentor = allmentors.find(m => m.id === 1);
+        const profile_section = document.querySelector('.profile-details');
+        if (profile_section !==null) {
+            showSlots(mentor,profile_section)
+        }
+    }
 });
+function showSlots(mentor,event){
+    const img_box = event.querySelector('.img-box img');
+    img_box.src=mentor.image;
+    const name_part = event.querySelector('.child-boxes2 .name-part');
+    name_part.textContent=mentor.name;
+    const slotSection = event.querySelector(".slot-box");
+    slotSection.innerHTML = '<h5 class="heading" >Book Your Slot</h5>';
+    if (slotSection !==null) {
+        const slot_book = document.createElement("div");
+        slot_book.classList.add("slot-book");
+        mentor.availableSlots.forEach(slot => {
+            const slotButton = document.createElement("button");
+            slotButton.setAttribute('type','button');
+            slotButton.innerHTML=`<span>Availabe</span><br>
+                            <span>${slot}</span>`;
+
+            // Check if slot is already booked
+            if (mentor.bookedSlots.includes(slot)) {
+                slotButton.classList.add('booked')
+                slotButton.innerHTML=`<span>Booked</span><br>
+                                <span>${slot}</span>`;
+            }
+
+            // Add event listener to book the slot
+            slotButton.addEventListener("click", () => {
+                const actionBut = bookSlot(mentor.id, slot);
+                if (actionBut) {
+                    slotButton.classList.add('booked');// Show confirmation alert
+                    alert(`Now, You have successfully booked ${slot} with ${mentor.name}.`);
+                }
+                else{
+                    alert('Something Went Wrong!!');
+                }
+                
+            });
+
+            slot_book.appendChild(slotButton);
+        });
+        slotSection.appendChild(slot_book);        
+    }
+}
 const mentors_profile=[
     {
         id:'1',
